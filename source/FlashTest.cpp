5ee48557c1f228fff1cee19ce19e04c2497eadcb
Alex Keating <alexnfk@gmail.com>
2025-11-25 14:08:08 +0000

    corrections of test prorgam

--- source/FlashTest.cpp
+++ source/FlashTest.cpp
@@ -1,11 +1,10 @@
-__attribute__((section(".smallprog"))) const uint8_t small_program[4096] = 
+#include <stdint.h>
+
+__attribute__((section(".smallprog"))) void test()
 {
-    void test()
+    volatile int x = 0;
+    while (1)
     {
-        volatile int x = 0;
-        while(1)
-        {
-            x++;
-        }
+        x++;
     }
-};
+}
